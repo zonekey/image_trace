eb97f5a61ea0fbb2acd7bfe436ddb45a4f1d86d6
sunkw <sunkwei@gmail.com>
2014-12-02 15:20:19 +0800

    ..

--- opencv/hog/train.sh
+++ opencv/hog/train.sh
@@ -1,1 +1,1 @@
-opencv_traincascade -vec faces_24_24.vec -bg bg.txt -info info.dat -numPos 1450 -numNeg 3000 -data cascade/ -precalcValBufSize 1024 -precalcIdxBufSize 1024 -featureType LBP
+opencv_traincascade -vec faces_24_24.vec -bg bg.txt -info info.dat -numPos 1450 -numNeg 2000 -data cascade/ -precalcValBufSize 2048 -precalcIdxBufSize 2048
